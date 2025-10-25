4a0893b194a10efd8f15f5951ee47175c0ef0e8e
SHAIK THAHA <shaikthaha@SHAIKs-MacBook-Air-2.local>
2025-10-25 20:13:50 +0530

    commit

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/HACKARE_3.0-registration/',
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
